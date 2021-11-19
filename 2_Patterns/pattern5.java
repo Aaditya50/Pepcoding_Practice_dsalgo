e04eb9d21a930aa2130bf6a7a2e20d43673971ea
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2021-11-19 12:47:33 +0530

    Create pattern5.java
    //OutPut
    		*	
    	*	*	*	
    *	*	*	*	*	
    	*	*	*	
    		*

---
+++ 2_Patterns/pattern5.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        int sp = n/2;
+        int st = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<sp;j++){
+                System.out.print("\t");
+            }
+            for(int j=0;j<st;j++){
+                System.out.print("*\t");
+            }
+            if(i<n/2){
+                sp--;
+                st+=2;
+            }else{
+                sp++;
+                st-=2;
+            }
+            System.out.println();
+        }
+
+    }
+}
