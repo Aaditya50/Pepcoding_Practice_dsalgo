1de99c323e807e208eb124564858b00dad1466d2
Aaditya Sudhir Joshi <64467870+Aaditya55@users.noreply.github.com>
2022-03-03 16:51:34 +0530

    Create Is Balanced Tree.java

---
+++ Binary Tree/Is Balanced Tree.java
@@ -0,0 +1,135 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static int height(Node node){
+    if(node == null) return -1;
+    int le = height(node.left);
+    int ri = height(node.right);
+    return Math.max(le,ri)+1;
+  }
+
+
+  public static class isBal{
+    int ht ;
+    boolean bal ;
+  }
+  public static isBal isBalance(Node node){
+    if(node == null){
+      isBal ans = new isBal();
+      ans.ht = -1;
+      ans.bal = true;
+      return ans;
+    }
+    isBal le = isBalance(node.left);
+    isBal ri = isBalance(node.right);
+
+    isBal res = new isBal();
+    res.ht = Math.max(le.ht,ri.ht)+1;
+
+    res.bal = le.bal && ri.bal && Math.abs(le.ht-ri.ht)<=1;
+    return res;
+
+
+   
+  }
+  
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    Node root = construct(arr);
+    
+    // write your code here
+    isBal ans =  isBalance(root);
+
+    System.out.println(ans.bal);
+  }
+
+}
